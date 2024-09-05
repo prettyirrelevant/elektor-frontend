b41fb3fc895e45eb956f89036d5f694faaf1db66
Isaac Adewumi <ienioladewumi@gmail.com>
2024-09-05 07:43:23 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import { Route, Routes } from "react-router-dom";
 import Election from "./Pages/Elections";
 import Home from "./Pages/Home";
-// import Dashboard from "./Pages/Dashboard";
 
 const App: React.FC = () => {
   return (
@@ -10,9 +9,6 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/elections" element={<Election />} />
-
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-
       </Routes>
     </>
   );
--- src/Pages/Home/Header/index.tsx
+++ src/Pages/Home/Header/index.tsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
 
-
   return (
     <div className="py-8 px-6">
       <div className="flex justify-between items-center">
